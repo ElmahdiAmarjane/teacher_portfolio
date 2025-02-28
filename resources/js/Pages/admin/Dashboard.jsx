8f7975736181c1dbf415800decd976b78f839664
amarjane <amarjaneelmahdi03@gmail.com>
2025-02-28 23:28:16 +0000

    login/logout redirection worked ✅

--- resources/js/Pages/admin/Dashboard.jsx
+++ resources/js/Pages/admin/Dashboard.jsx
@@ -1,3 +1,5 @@
+import ResponsiveNavLink from "@/Components/ResponsiveNavLink";
+import Authenticated from "@/Layouts/AuthenticatedLayout";
 
 
 
@@ -5,8 +7,17 @@ const Dashboard = ()=>{
 
     return (
      <>
+   
+        
+      <h1 className="text-red-700">HELLOW FROM DASH</h1>  
+
     
-      <h1 className="text-red-700">HELLOW FROM DASH</h1>    
+                            <div className="mt-3 space-y-1">
+                                <ResponsiveNavLink href={route('profile.edit')}>Profile</ResponsiveNavLink>
+                                <ResponsiveNavLink method="post" href={route('logout')} as="button">
+                                    Log Out
+                                </ResponsiveNavLink>
+                            </div>
 
     </>
     )
--- routes/web.php
+++ routes/web.php
@@ -21,57 +21,63 @@ use Inertia\Inertia;
 // });
 
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
 
 
-Route::get('/signup', function () {
-    return Inertia::render('Signup');
-});
-
-Route::get('/home', function () {
-    return Inertia::render('Home');
-});
-
-// Route::get('/dashboard', function () {
-//     return Inertia::render('Dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-// ADD ROUTES 
-
-// Student Pages (With Header & Footer)
-Route::get('/', function () {
+   // Student Pages (With Header & Footer)
+   Route::get('/', function () {
     return Inertia::render('student/Home', [
         'layout' => 'student', // Pass the layout name as a prop
     ]);
 });
 
-// Admin Pages (With Sidebar)
-Route::prefix('admin')->group(function () {
-    Route::get('/', function () {
-        return Inertia::render('admin/Dashboard', [
-            'layout' => 'admin', // Pass the layout name as a prop
-        ]);
-    })->middleware(['auth', 'verified'])->name('admin.dashboard');
-
-    Route::get('/addCourse', function () {
-        return Inertia::render('admin/AddCourse', [
-            'layout' => 'admin', // Pass the layout name as a prop
-        ]);
-    })->middleware(['auth', 'verified'])->name('admin.addCourse');
+Route::get('/signup', function () {
+    return Inertia::render('Signup');
 });
+
+// Route::get('/home', function () {
+//     return Inertia::render('Home');
+// });
+
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+ 
+    
+    // Admin Pages (With Sidebar)
+    Route::prefix('admin')->group(function () {
+        Route::get('/', function () {
+            return Inertia::render('admin/Dashboard', [
+                'layout' => 'admin',
+            ]);
+        })->name('admin.dashboard');
+
+        Route::get('/addCourse', function () {
+            return Inertia::render('admin/AddCourse', [
+                'layout' => 'admin',
+            ]);
+        })->name('admin.addCourse');
+    });
+
+});
+
+
+// ADD ROUTES 
 
 // Minimal Pages (No Sidebar, No Header/Footer)
 // Route::get('/login', function () {
@@ -88,4 +94,4 @@ Route::fallback(function () {
 });
 
 
-require __DIR__.'/auth.php';
+
